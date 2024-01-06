f094c3b0b436d7543eca2e3d8a4b47f384932fee
Alireza Bashiri <al3rez@gmail.com>
2024-01-06 14:59:48 +0700

    progress commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,27 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user.blank?
+      render :new
+      flash.now[:alert] = 'Invalid email or password'
+      return
+    end
+
+    if user.authenticate(params[:password])
+      reset_session
+      session[:current_user_id] = user.id
+      redirect_to listings_path, notice: 'Successfully logged in.'
+    else
+      flash.now[:alert] = 'Invalid email or password'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
